aec17cd771691afba1bd5276a2883f4df972537e
Léo DESIGAUX <leodesigaux@gmail.com>
2022-03-03 00:11:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const {
 
 let browser = null;
 let page = null;
-const isHeadless = false;
+const isHeadless = true;
 const userAgent ="Mozilla/5.0 (X11; Linux x86_64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
 
 
